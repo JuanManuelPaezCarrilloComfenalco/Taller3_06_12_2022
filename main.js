e3251ca8c12d00be9e9a84b6a8f79032aad296cb
JuanManuelPaezCarrilloComfenalco <paez3939@gmail.com>
2022-12-07 21:32:07 -0500

    solucion 1

---
+++ main.js
@@ -0,0 +1,31 @@
+addEventListener("DOMContentLoaded", (e)=>{ 
+   
+
+   let numero1=prompt('Digita un numero')
+   alert("el numero ingresado es:" + numero1)
+   
+   let numero2=prompt('digita un segundo numero')
+   alert("el segundo numero ingresado es:" + numero2)
+
+   if(numero1 > numero2){
+   alert("el numero mayor es" + numero1)
+   }
+   else {
+      alert("el numero mayor es" + numero2)
+   }
+    
+   
+
+   
+   
+   
+      
+   
+   
+})
+    
+
+
+    
+
+ 
