086f152b00a1f111fe7aa7ab79701f49f1030c3d
JuanManuelPaezCarrilloComfenalco <paez3939@gmail.com>
2022-12-09 14:22:32 -0500

    solucion 4

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-addEventListener("DOMContentLoaded", (e)=>{ 
+addEventListener("DOMContentLoaded", (e)=>{
+// Para insertar datos debe escribirlos en la linea de console.log
+// inmediatamente los datos se imprimirán en la consola del navegador
+  function obtenerNumeroMayor (a,b,c){
+    let mayor=0;
+    
+    if(a > b){
+        mayor=a;
+    }else{
+        mayor = b
+    }
+    if (c > mayor){
+        mayor = c
+    }
+    return mayor;
+  }
+  
+  console.log(obtenerNumeroMayor(400, 121, 21));
+})
+
+   
    
 
- function numerosPares(){
-   for (let index = 2; index <=100; index += 2){
-      console.log(index);
-   }
- }
- 
- numerosPares();
-})
     
 
 
