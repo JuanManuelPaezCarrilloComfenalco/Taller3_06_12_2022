16674f3304f3be0b1e1a1796eb86027285a17f98
JuanManuelPaezCarrilloComfenalco <paez3939@gmail.com>
2022-12-13 18:35:36 -0500

    solucion 6

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
 addEventListener("DOMContentLoaded", (e)=>{
-// Para insertar datos debe escribirlos en la linea de console.log
-// inmediatamente los datos se imprimirán en la consola del navegador
-  function obtenerNumeroMayor (a,b,c){
-    let mayor=0;
-    
-    if(a > b){
-        mayor=a;
-    }else{
-        mayor = b
-    }
-    if (c > mayor){
-        mayor = c
-    }
-    return mayor;
-  }
-  
-  console.log(obtenerNumeroMayor(400, 121, 21));
-})
 
+  
+  let suma  
+  let producto 
+
+  parseInt(prompt("digite un numero"))
+  parseInt(prompt("digiite otro numero"))
+
+  if('numero es par'){
+    console.log(suma)
+  }
+    else{
+    console.log(producto)
+  }          
+})
    
    
 
