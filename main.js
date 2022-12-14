83c852f0c249f73a7c9f22f09107a27712b53057
JuanManuelPaezCarrilloComfenalco <paez3939@gmail.com>
2022-12-13 19:34:25 -0500

    taller3_06_12_2022

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 addEventListener("DOMContentLoaded", (e)=>{
 
   
-  let suma  
-  let producto 
-
-  parseInt(prompt("digite un numero"))
-  parseInt(prompt("digiite otro numero"))
-
-  if('numero es par'){
-    console.log(suma)
+  let lista = [];
+  let suma = 0;
+  let mult = 1;
+  while(confirm("Desea ingresar datos")){
+      lista.push(prompt());
   }
-    else{
-    console.log(producto)
-  }          
+  for (let i = 0; i < lista.length; i++) {
+      if(lista[i] % 2 == 0){
+          suma += Number(lista[i]);
+      }else{
+          mult *= Number(lista[i]);
+      }
+  }
+  console.log(`La suma de los pares es: ${suma}`);
+  console.log(`El producto de los impares es: ${mult}`);
+  console.log(lista);
+
 })
    
    
