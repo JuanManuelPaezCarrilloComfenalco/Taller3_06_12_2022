c28ddb2e963841d833417700a62dac09580e2d65
JuanManuelPaezCarrilloComfenalco <paez3939@gmail.com>
2022-12-13 20:55:08 -0500

    solucion 7 terminado

--- main.js
+++ main.js
@@ -1,31 +1,18 @@
 addEventListener("DOMContentLoaded", (e)=>{
 
-  // Función que determina si un número es perfecto
-function esPerfecto(n) {
-  // Suma de los divisores del número (excluyendo el propio número)
-  var suma = 0;
 
-  // Recorremos todos los posibles divisores del número
-  for (var i = 1; i < n; i++) {
-    // Si el número es divisible por i, lo añadimos a la suma
-    if (n % i == 0) {
-      suma += i;
-    }
-  }
-  // Si la suma de los divisores es igual al propio número, es perfecto
-  if (suma == n) {
-    return true;
-  } else {
-    return false;
+
+let num = Number(prompt("digite un numero"));
+
+let divisorsSum = 0;
+for (let i = 1; i < num; i++) {
+  if (num % i == 0) {
+    divisorsSum += i;
   }
 }
-// Ejemplo: Comprobamos si el número 28 es perfecto
-if (esPerfecto(21)) {
-  console.log("El número 28 es perfecto");
+if (divisorsSum == num) {
+  console.log(`${num} es un número perfecto.`);
 } else {
-  console.log("El número 28 no es perfecto");
+  console.log(`${num} no es un número perfecto.`);
 }
-
 })
-
- 
